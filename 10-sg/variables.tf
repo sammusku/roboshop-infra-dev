3e3bd94ad3d94ffd14173ff1e38ce6c1b7aac5d6
sammusku <Samyuktha.musku@gmail.com>
2026-03-04 14:14:49 -0600

    roboshop

--- 10-sg/variables.tf
+++ 10-sg/variables.tf
@@ -6,3 +6,20 @@ variable "environment" {
     type = string
     default = "dev"
 }
+variable "sg_names" {
+    type = list
+    default = [
+        #Databases
+        "mongodb","redis","mysql","rabbitmq",
+        #Backend
+        "catalogue","user","cart","shipping","payment",
+        #Backend-ALB
+        "backend_alb",
+        #Frontend
+        "frontend",
+        #Frontend-ALD
+        "frontend_alb",
+        #Bastion
+        "bastion"
+    ]
+}
