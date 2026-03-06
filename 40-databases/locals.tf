52edaab5464f4549433d6535016b6bd71cd2918c
sammusku <Samyuktha.musku@gmail.com>
2026-03-06 11:11:19 -0600

    terraform

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -1,13 +1,14 @@
 locals {
-      ami_id = data.aws_ami.devops
+      ami_id = data.aws_ami.devops.id
 
       common_tags = {
         project = var.project
         environment = var.environment
         terraform = true
       }
-      
+
       database_subnet_ids = split(",", data.aws_ssm_parameter.database_subnet_ids.value)[0] # converting stringList to list and retrieved  az-1a. 
       mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
 }
-    
+
+
