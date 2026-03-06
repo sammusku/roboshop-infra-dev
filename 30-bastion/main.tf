6c5660c7dfb3d09e48c9dbc31efedda8705d3573
sammusku <Samyuktha.musku@gmail.com>
2026-03-06 12:33:17 -0600

    terraform

--- 30-bastion/main.tf
+++ 30-bastion/main.tf
@@ -4,6 +4,18 @@ resource "aws_instance" "bastion" {
     subnet_id = local.public_subnet_id
     vpc_security_group_ids = [local.bastion_sg_id]
     iam_instance_profile = aws_iam_instance_profile.bastion.name
+    root_block_device {
+      volume_size = 50  # Sets the root volume size to 50 GiB
+      volume_type = "gip3"
+      #EBS volume tags
+      tags = merge (
+        {
+        Name = "${var.project}-${var.environment}-bastion"
+        },
+    
+      local.common_tags
+      )
+   }
 
     tags = merge (
         {
