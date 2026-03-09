df3856b4c242ab4f9e39a26569f00d326a45de03
sammusku <Samyuktha.musku@gmail.com>
2026-03-09 11:25:14 -0600

    terraform

---
+++ 40-databases/iam.tf
@@ -0,0 +1,25 @@
+resource "aws_iam_role" "test_role" {
+  name = local.mysql_role_name  #Roboshop-Dev-Mysql
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge (
+  {
+    Name = local.mysql_role_name
+  },
+  local.common_tags)
+}
