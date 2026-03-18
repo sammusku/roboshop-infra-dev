3eec52d80beee2d167e4fd1beb86fd2a149520d3
sammusku <Samyuktha.musku@gmail.com>
2026-03-18 15:59:10 -0600

    terraform

--- 20-sg-rules/data.tf
+++ 20-sg-rules/data.tf
@@ -29,3 +29,9 @@ data "aws_ssm_parameter" "rabbitmq_sg_id" {
 data "aws_ssm_parameter" "backend_alb_sg_id" {
     name = "/${var.project}/${var.environment}/backend_alb_sg_id"
 }
+data "aws_ssm_parameter" "frontend_alb_sg_id" {
+    name = "/${var.project}/${var.environment}/frontend_alb_sg_id"
+}
+data "aws_ssm_parameter" "frontend_sg_id" {
+    name = "/${var.project}/${var.environment}/frontend_sg_id"
+}
